9140ce3d4bda6f3a33189678f5cfcf6f6d20591e
VranceanuM <markdevstudio@gmail.com>
2018-03-06 16:52:39 +0300

    Add files via upload

---
+++ Reddid/redditapi.js
@@ -0,0 +1,9 @@
+export default {
+    search:function(searchTerm,sortBy,searchLimit){
+      return  fetch(`http://www.reddit.com/search.json?q=${searchTerm}
+        &sort=${sortBy}&limit=${searchLimit}`)
+          .then(res => res.json())
+          .then(data =>data.data.children.map(data => data.data)
+        );
+    }
+}
